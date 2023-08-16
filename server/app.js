423b31b4e8dcfafc15b2632981200aac637210f2
AnthonyFebles <anthonyfebles21@outlook.com>
2023-08-15 21:16:18 -0400

    progress made

--- server/app.js
+++ server/app.js
@@ -4,5 +4,5 @@ const app = express();
 const peopleRouter = require('./routes/people');
 app.use('/people', peopleRouter);
 
-const port = 5000;
+const port = 8080;
 app.listen(port, () => console.log('Server is listening on port', port));
---
+++ server/routes/color.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+
+router.get("/", (req,res) =>{
+    res.json("GET /colors")
+})
+
+router.get("/:name", (res, req)=> {
+    res.json("GET /colors/:name")
+} )
